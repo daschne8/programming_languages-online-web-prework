04f732e6b3bca0f9ea73351a517d2d711bb250e8
Daniel Schneider <daschne8@gmail.com>
2019-03-05 18:37:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style,style_languages|
     style_languages.each do |language,lan_data|
-      lan_data[:style] = [] << style.to_s
+      lan_data[:style] = [] << style
       new_hash[language] = lan_data
     end
   end
